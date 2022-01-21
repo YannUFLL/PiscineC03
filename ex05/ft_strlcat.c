fe27237accd0489b0893eeabce33bc3a818bbd73
Yann Dumaine <yannlescoul79f>
2022-01-21 13:52:16 +0100

    ex5 OK

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -1,28 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 17:53:29 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/01/20 17:57:06 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/01/21 13:47:47 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncat(char *dest, char *src, unsigned int nb)
+unsigned int	*ft_strlcat(char *dest, char *src, unsigned int nb)
 {
 	int unsigned	i;
 	int unsigned	u;
+	int unsigned	r;
 
 	i = 0;
 	u = 0;
+	r = 0;
 	while (dest[i])
 		i++;
-	while (src[u] && u < nb)
-	{
-		dest[i + u] = src[u];
+	while (src[u])
 		u++;
+	if ((i + u) < nb)
+	{	
+		while (src[u])
+		{
+			dest[i + u] = src[u];
+			r++;
+		}
 	}
-	return (dest);
+	return (i + u);
 }
